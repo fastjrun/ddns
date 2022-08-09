6ac0bf8b446beb644a5ac2b3f70601941f88ed5b
fastjrun <fastjrun@139.com>
2022-08-09 23:36:50 +0800

    引入dns-cache-manipulator

--- src/main/java/com/fastjrun/util/IpUtil.java
+++ src/main/java/com/fastjrun/util/IpUtil.java
@@ -1,19 +1,24 @@
 package com.fastjrun.util;
 
+import com.alibaba.dcm.DnsCacheManipulator;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public interface IpUtil {
 
-  String locateWanIP();
+    String locateWanIP();
 
-  default String getIPByDomain(String domain) {
-    try {
-      InetAddress myServer = InetAddress.getByName(domain);
-      return myServer.getHostAddress();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
+    default String getIPByDomain(String domain) {
+        // 清除缓存
+        DnsCacheManipulator.removeDnsCache(domain);
+        // 根据域名获取IP
+        try {
+            InetAddress myServer = InetAddress.getByName(domain);
+            return myServer.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
-    return "";
-  }
 }
