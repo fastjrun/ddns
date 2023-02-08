ab605d83b0375ce3e75458fbc655ff4e18d8b1d9
fastjrun <fastjrun@139.com>
2023-02-09 07:18:49 +0800

    引入mysql驱动

--- docker.groovy
+++ docker.groovy
@@ -17,8 +17,8 @@ node {
                     dir('output') {
                         unstash 'tarOutput'
                     }
-                    sh 'cd output && docker build . -t pi4k8s/ddns:3.1.1-amd64'
-                    sh 'docker push pi4k8s/ddns:3.1.1-amd64'
+                    sh 'cd output && docker build . -t pi4k8s/ddns:3.2-amd64'
+                    sh 'docker push pi4k8s/ddns:3.2-amd64'
                 },
                 'docker build && push arm64': {
                     node('arm64') {
@@ -26,8 +26,8 @@ node {
                         dir('output') {
                             unstash 'tarOutput'
                         }
-                        sh 'cd output && docker build . -t pi4k8s/ddns:3.1.1-arm64'
-                        sh 'docker push pi4k8s/ddns:3.1.1-arm64'
+                        sh 'cd output && docker build . -t pi4k8s/ddns:3.2-arm64'
+                        sh 'docker push pi4k8s/ddns:3.2-arm64'
                     }
 
                 }
@@ -36,13 +36,13 @@ node {
     }
     stage('manifest'){
         try {
-            sh 'docker manifest rm pi4k8s/ddns:3.1.1'
+            sh 'docker manifest rm pi4k8s/ddns:3.2'
         }catch(exc){
             echo "some thing wrong"
         }
-        sh 'docker manifest create pi4k8s/ddns:3.1.1 pi4k8s/ddns:3.1.1-amd64 pi4k8s/ddns:3.1.1-arm64'
-        sh 'docker manifest annotate pi4k8s/ddns:3.1.1 pi4k8s/ddns:3.1.1-amd64 --os linux --arch amd64'
-        sh 'docker manifest annotate pi4k8s/ddns:3.1.1 pi4k8s/ddns:3.1.1-arm64 --os linux --arch arm64'
-        sh 'docker manifest push pi4k8s/ddns:3.1.1'
+        sh 'docker manifest create pi4k8s/ddns:3.2 pi4k8s/ddns:3.2-amd64 pi4k8s/ddns:3.2-arm64'
+        sh 'docker manifest annotate pi4k8s/ddns:3.2 pi4k8s/ddns:3.2-amd64 --os linux --arch amd64'
+        sh 'docker manifest annotate pi4k8s/ddns:3.2 pi4k8s/ddns:3.2-arm64 --os linux --arch arm64'
+        sh 'docker manifest push pi4k8s/ddns:3.2'
     }
 }
